929e0141b9b7e915c85cb6ac5dd40355246aac4e
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-20 10:47:47 -0800

    added answer stuff

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,51 @@
 class AnswersController < ApplicationController
+  def index
+    @answers = Answer.all
+  end
+
+  def show
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+     @question = Question.find(params[:question_id])
+     @answer = @question.answers.create(answer_params)
+     if @answer.save
+        redirect_to question_path(@question)
+     end
+  end
+
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find([:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find([:id])
+
+    if @answer.update(answer_params)
+      redirect_to question_path(@question)
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find([:id])
+    @answer.destroy
+
+    redirect_to question_path(@question)
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
+
+
   root 'questions#index'
 
 
